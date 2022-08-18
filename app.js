0c3369b6db362e7fb44d544381ee226584b8ff05
walidmh18 <walidmh183.com>
2022-08-18 01:34:57 +0100

    fixes

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+/* eslint-disable */
 hljs.highlightAll();
 
 let firstPage = document.querySelector(".firstPage");
@@ -701,20 +702,14 @@ function applyCss() {
 function getStyle(el, styleProp) {
   var value,
     defaultView = (el.ownerDocument || document).defaultView;
-  // W3C standard way:
   if (defaultView && defaultView.getComputedStyle) {
-    // sanitize property name to css notation
-    // (hypen separated words eg. font-Size)
     styleProp = styleProp.replace(/([A-Z])/g, "-$1").toLowerCase();
     return defaultView.getComputedStyle(el, null).getPropertyValue(styleProp);
   } else if (el.currentStyle) {
-    // IE
-    // sanitize property name to camelCase
-    styleProp = styleProp.replace(/\-(\w)/g, function (str, letter) {
+    styleProp = styleProp.replace(/-(\w)/g, function (str, letter) {
       return letter.toUpperCase();
     });
     value = el.currentStyle[styleProp];
-    // convert other units to pixels on IE
     if (/^\d+(em|pt|%|ex)?$/i.test(value)) {
       return (function (value) {
         var oldLeft = el.style.left,
